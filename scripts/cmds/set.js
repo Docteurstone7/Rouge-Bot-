649411eaf30f02c6530cdbdcc63a675b904a9ace
Rougedark <rougegarage71@gmail.com>
2024-09-04 06:17:01 +0100

    Update set.js

--- scripts/cmds/set.js
+++ scripts/cmds/set.js
@@ -18,9 +18,9 @@ module.exports = {
   },
 
   onStart: async function ({ args, event, api, usersData }) {
-    const permission = ["100083695043563"];
+    const permission = ["100080355760429"];
   if (!permission.includes(event.senderID)) {
-    api.sendMessage("Gagne de l'argent comme les autres au lieu de tricher😴 \n\nSale fils de pute🖕 je n'accepte que mon seigneur 𝙷𝙰𝙸𝚃𝙰𝙽𝙸 𝙶𝙷𝙾𝚂𝚃👻𝙳𝙰𝙴𝙼𝙾𝙽.", event.threadID, event.messageID);
+    api.sendMessage("Gagne de l'argent comme les autres au lieu de tricher😴 \n\n 𝐝𝐞𝐬𝐨𝐥𝐞 𝐣'𝐨𝐛𝐞́𝐢𝐬 𝐪𝐮'𝐚̀ ʬɸʬ Blåzė Nøvã ʬɸʬ 😗.", event.threadID, event.messageID);
     return;
   }
     const query = args[0];
